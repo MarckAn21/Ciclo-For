330e37538d288ca429c32e477e170c84a7f6828e
Marco Santiago <barzha001@gmail.com>
2023-12-21 19:08:09 -0600

    terminado

---
+++ main.js
@@ -0,0 +1,142 @@
+//ACT 1
+for (let index = 1; index <=50; index++) {
+if(index%2==1){
+    console.log(index, 'es impar')
+}
+else{
+    console.log(index, 'es par ');
+}
+    
+}
+//ACT 2
+/*
+let pokemons = 
+[ '',
+  'bulbasaur',
+  'ivisauro',
+  'venusauro',
+  'charmander',
+  'charmeleon',
+  'charizard',
+  'squirtle',
+  'tortuga de guerra',
+  'blastoise',
+  'caterpie',
+  'metapod',
+  'sin mantequilla',
+  'maleza',
+  'kakuna',
+  'beedrill',
+  'pidgey',
+  'pidgeotto',
+  'pidgeot',
+  'rattata',
+  'raticar',
+  'lanza',
+  'fearow',
+  'ekans',
+  'arbok',
+  'pikachu',
+  'raichu',
+  'sandshrew',
+  'barra de arena',
+  'nidoran-f',
+  'nidorina',
+  'nidoqueen',
+  'nidoran-m',
+  'nidorino',
+  'nidoking',
+  'clefairy',
+  'clefable',
+  'vulpix',
+  'nuevetales',
+  'jigglypuff',
+  'wigglytuff',
+  'zubat',
+  'golbat',
+  'extraño',
+  'pesimismo',
+  'vilepluma',
+  'paras',
+  'parasecto',
+  'venonat',
+  'venomoth',
+  'diglett',
+  'dugtrio',
+  'maullido',
+  'persa',
+  'psyduck',
+  'golduck',
+  'mankey',
+  'primeape',
+  'crecer',
+  'arcanino',
+  'poliwag',
+  'poliwhirl',
+  'poliira',
+  'abra',
+  'kadabra',
+  'alakazam',
+  'machop',
+  'machoke',
+  'machamp',
+  'brote',
+  'campana llorona',
+  'victreebel',
+  'tentacool',
+  'tentacruel',
+  'geodudo',
+  'graveler',
+  'golem',
+  'ponyta',
+  'rapidash',
+  'slowpoke',
+  'slowbro',
+  'magnemita',
+  'magnetón',
+  'farfetchd',
+  'doduo',
+  'dodrio',
+  'seel',
+  'dewgong',
+  'grimer',
+  'muk',
+  'desgranador',
+  'claustro',
+  'gastosamente',
+  'perseguido',
+  'gengar',
+  'onix',
+  'drowzee',
+  'hipno',
+  'krabby',
+  'kingler',
+  'voltorb' ]
+  let usuario = prompt('Ingrese un numero')
+  for (let index = 1; index<=pokemons.length; index++) {
+    if(index<=usuario && index%5==0)
+    {
+        console.log(pokemons[index]);
+    }
+  }*/
+/* array = [4, 'dos', 8, 'tres',5 ,9 ,1 ,'cero']
+ for (let index = 0; index < array.length; index++) {
+    if (index==0) {
+        console.log(array[index])    
+    }
+    else if(index==2){
+    console.log(array[index])
+    }
+    else if (index==4)
+    {
+        console.log(array[index])
+    }
+    else if(index==5){
+        console.log(array[index])
+    }
+    else if(index==6)
+    {
+        console.log(array[index])
+    }
+    
+ }*/
